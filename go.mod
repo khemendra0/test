288650df8dd50269b4c5d1dbf2a813cc72124a08
khemendra kumar <khemendra.kumar13@gmail.com>
2022-08-21 19:46:40 +0530

    Added test pkg
    Signed-off-by: khemendra kumar <khemendra.kumar13@gmail.com>

---
+++ go.mod
@@ -0,0 +1,5 @@
+module khemendra0/test
+
+go 1.19
+
+require github.com/khemendra0/test v0.0.0-20220820134031-46e485076243 // indirect
---
+++ main.go
@@ -0,0 +1,9 @@
+package test
+
+import "fmt"
+
+func greetings() string {
+	var name string
+	msg := fmt.Sprintf("welcome %v indian", name)
+	return msg
+}
